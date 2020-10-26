cab7936d34982288fdcdc4e8f2965fe6349b8712
MihaiB11 <mihai.mbejan@yahoo.com>
2020-10-26 21:13:37 +0200

    Database Web App.Storing User Data to the Database

--- Database Web App/app.py
+++ Database Web App/app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:postgres123@localhost/Height collector'
+db = SQLAlchemy(app)
+
+class Data(db.Model):
+    __tablename__="data"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    height = db.Column(db.Integer)
+
+    def __init__(self, email, height):
+        self.email = email
+        self.height = height
 
 @app.route("/")
 def index():
@@ -12,7 +25,12 @@ def success():
         email = request.form["email_name"]
         height = request.form['height_name']
         print(email, height)
-        return render_template("success.html")
+        if db.session.query(Data).filter(Data.email == email).count() == 0:
+            data = Data(email, height)
+            db.session.add(data)
+            db.session.commit()
+            return render_template("success.html")
+    return render_template("index.html", text = "Email address already stored in the database!")
 
 if __name__ == '__main__':
     app.debug = True
